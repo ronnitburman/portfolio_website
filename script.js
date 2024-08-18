c3e7eebddca32c857aba7374f7765abc198b678b
Ronnit Roy Burman <roburman@cisco.com>
2024-08-18 19:14:27 -0400

    create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Data Scientist", "Innovator", "Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
